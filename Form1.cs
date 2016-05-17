3d005723a93a96778234e68324b970402e30c049
Jonathan C <jcc064@lionmail.lindenwood.edu>
2016-05-17 15:08:29 -0500

    Getting there...

--- Form1.cs
+++ Form1.cs
@@ -101,7 +101,7 @@ namespace WindowsFormsApplication6
                             }
 			            }        
 	            }
-                    }
-	            }
-            };
+         }
+	}
+};
 
